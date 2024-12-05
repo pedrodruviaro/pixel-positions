d3109380e58b6de3eb9a406f0110e6204e1a2b60
pedrodruviaro <pedrodruviaro@gmail.com>
2024-12-05 09:09:11 -0300

    feat: first components and initial home layout

---
+++ resources/views/components/section-heading.blade.php
@@ -0,0 +1,4 @@
+<div class="flex items-center gap-2">
+    <span class="w-2 h-2 bg-white inline-block"></span>
+    <h2 class="font-bold text-lg">{{ $slot }}</h2>
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,35 @@
 <x-layout>
-    Hello world!
+    <div class="space-y-10">
+        <section>
+            <x-section-heading>Features Jobs</x-section-heading>
+
+            <div class="mt-6 grid lg:grid-cols-3 gap-8">
+                <x-job-card />
+                <x-job-card />
+                <x-job-card />
+            </div>
+        </section>
+
+        <section>
+            <x-section-heading>Tags</x-section-heading>
+
+            <div class="mt-6 flex flex-wrap gap-2">
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+            </div>
+        </section>
+
+        <section>
+            <x-section-heading>Recent Jobs</x-section-heading>
+        </section>
+    </div>
 </x-layout>
