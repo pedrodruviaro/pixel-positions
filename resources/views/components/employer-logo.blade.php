de4f978b988ae768b55f4de0c4a7c38d644f92f5
pedrodruviaro <pedrodruviaro@gmail.com>
2024-12-10 11:16:16 -0300

    fix: employer logo path

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,3 +1,3 @@
-@props(['width' => 90])
+@props(['employer', 'width' => 90])
 
-<img src="http://picsum.photos/seed/{{ rand(0, 1000) }}/{{ $width }}" alt="" class="rounded-xl">
+<img src="{{ asset($employer->logo) }}" width="{{ $width }}" alt="" class="rounded-xl">
